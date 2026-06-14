bfcbe94faa446bb97e4b695b44433abc5aa954cf
user <user@mcpcraft.dev>
2026-06-14 17:02:47 +0300

    feat: redesign docs with Better Auth-inspired layout, add overview and comparison pages

--- website/.source/server.ts
+++ website/.source/server.ts
@@ -1,11 +1,13 @@
 // @ts-nocheck
-import * as __fd_glob_9 from "../content/docs/examples/gmail-server.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/examples/basic-server.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/api/tool.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/api/resource.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/api/create-server.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/quick-start.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/examples/gmail-server.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/examples/basic-server.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/api/tool.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/api/resource.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/api/create-server.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/quick-start.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/comparison.mdx?collection=docs"
 import { default as __fd_glob_2 } from "../content/docs/examples/meta.json?collection=docs"
 import { default as __fd_glob_1 } from "../content/docs/api/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
@@ -17,4 +19,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api/meta.json": __fd_glob_1, "examples/meta.json": __fd_glob_2, }, {"installation.mdx": __fd_glob_3, "quick-start.mdx": __fd_glob_4, "api/create-server.mdx": __fd_glob_5, "api/resource.mdx": __fd_glob_6, "api/tool.mdx": __fd_glob_7, "examples/basic-server.mdx": __fd_glob_8, "examples/gmail-server.mdx": __fd_glob_9, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api/meta.json": __fd_glob_1, "examples/meta.json": __fd_glob_2, }, {"comparison.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "installation.mdx": __fd_glob_5, "quick-start.mdx": __fd_glob_6, "api/create-server.mdx": __fd_glob_7, "api/resource.mdx": __fd_glob_8, "api/tool.mdx": __fd_glob_9, "examples/basic-server.mdx": __fd_glob_10, "examples/gmail-server.mdx": __fd_glob_11, });
